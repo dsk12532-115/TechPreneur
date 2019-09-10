d281eb9eb4d6f8135600363edf5095b7825fccc6
田中 大奨 <>
2019-09-10 15:51:01 +0900

    データベースが壊れる前にプッシュ。models.pyに駅データを追加した。駅データはある程度入力した。

--- expert_proj/expert/admin.py
+++ expert_proj/expert/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Profile, Vacant_Seats
+from .models import Profile, Vacant_Seats, Station
 
 admin.site.register(Profile)
 admin.site.register(Vacant_Seats)
+admin.site.register(Station)
